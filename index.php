7891fb655845f996b2fe88aa3c3d8076313d5be0
harisanwar129 <harisanwar129@gmail.com>
2019-08-12 16:59:03 +0700

    Menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,9 @@
 <body>
     <h1>Hello world</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam necessitatibus nihil commodi, molestias debitis ratione eos ad fugit alias saepe non officiis magnam illum soluta quasi vero nisi adipisci explicabo!</p>
+<ul>
+<li>satu </li>
+<li>dua </li>
+</ul> 
 </body>
 </html>
