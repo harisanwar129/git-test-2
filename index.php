24bd6d020daa8fe19042ef592cc9aaef3e7b13e5
harisanwar129 <harisanwar129@gmail.com>
2019-08-12 16:48:58 +0700

    menambahakan paragraf

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 </head>
 <body>
     <h1>Hello world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam necessitatibus nihil commodi, molestias debitis ratione eos ad fugit alias saepe non officiis magnam illum soluta quasi vero nisi adipisci explicabo!</p>
 </body>
 </html>
